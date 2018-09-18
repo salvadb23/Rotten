e260889d632f1d5c57467eafd7e1e83c6ee589ea
Salvador Becerra <salvadorbecerra@Salvadors-MacBook-Pro.local>
2018-09-17 21:46:44 -0700

    users can create reviews

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ var exphbs = require('express-handlebars');
 //   { title: "Great Review" },
 //   { title: "Next Review" }
 // ]
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
 const Review = mongoose.model('Review', {
-  title: String
+  title: String,
+  description: String,
+  movieTitle: String
 });
+
 
 // app.get('/', (req, res) => {
 //   res.render('home', { msg: 'Hello World!' });
 // })
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   Review.find()
     .then(reviews => {
@@ -24,9 +28,24 @@ app.get('/', (req, res) => {
       console.log(err);
     })
 })
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+// app.post('/reviews', (req, res) => {
+//   console.log(req.body);
+//   // res.render('reviews-new', {});
+// })
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
