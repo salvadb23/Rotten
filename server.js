674882644089d5bfbb8a902346fc8c038566c6d7
Salvador Becerra <salvadorbecerra@Salvadors-MacBook-Pro.local>
2018-09-23 20:21:17 -0700

    start

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const app = express()
 var exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
-var port = process.env.PORT 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes');
 const methodOverride = require('method-override')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
@@ -15,7 +14,7 @@ reviews(app);
 
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('App listening on port 3000!')
 })
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
